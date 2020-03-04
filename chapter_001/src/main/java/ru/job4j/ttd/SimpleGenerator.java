cd19c1f33a237dbdbd4ee781edf48806e11e5c0d
Maxim Dick <maxim1994barca@gmail.com>
2020-03-04 23:56:50 +0300

    Исправить код класс SimpleGenerator.[#241641]

---
+++ chapter_001/src/main/java/ru/job4j/ttd/SimpleGenerator.java
@@ -0,0 +1,63 @@
+package ru.job4j.ttd;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SimpleGenerator implements Template {
+
+    /**
+     * JAVADOC - https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#compile(java.lang.String)
+     * Compiles the given regular expression into a pattern with the given flags.
+     * Компилирует данное регулярное выражение в шаблон с заданными флагами.
+     * */
+    private final static Pattern KEYS = Pattern.compile("\\$\\{.+?\\}");
+
+    @Override
+    public String generate(String template, Map<String, String> data) {
+        Matcher matcher = KEYS.matcher(template);
+        while (matcher.find()) {
+            String key = matcher.group();
+            if (!data.containsKey(key)) {
+                try {
+                    throw new Exception("Map does't contain that keys");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (!data.get(matcher.group()).equals(key)) {
+                    new RuntimeException("invalid parameters");
+            }
+            template = template.replaceFirst(KEYS.pattern(), data.get(matcher.group()));
+        }
+        return template;
+    }
+
+//    private final Pattern pattern = Pattern.compile("\\$\\{.+?\\}");
+//
+//    /**
+//     * Method search ${} and replace from collections map
+//     * @param template
+//     * @param data
+//     * @return
+//     */
+//    @Override
+//    public String generate(String template, Map<String, String> data) {
+//        Matcher matcher = pattern.matcher(template);
+//        while (matcher.find()) {
+//            String key = matcher.group();
+//            if (!data.containsKey(key)) {
+//                try {
+//                    throw new Exception("Map does't contain that key");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (!data.get(matcher.group()).equals(key)) {
+//                new RuntimeException("Unnecessary Param ");
+//            }
+//            template = template.replaceFirst(pattern.pattern(), data.get(matcher.group()));
+//        }
+//        return template;
+//    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/ttd/SimpleGeneratorTest.java
@@ -0,0 +1,95 @@
+package ru.job4j.ttd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class SimpleGeneratorTest {
+
+
+        @Test
+        public void whenTakeTextWithDataShouldReplaceParamsToData2() {
+            //assign
+            Template template = new SimpleGenerator();
+            String text = "Hello, ${name} ${surname}";
+            Map<String, String> data = new HashMap<>();
+            data.put("${name}", "Ivan");
+            data.put("${surname}", "Pupkin");
+            String expect = "Hello, Ivan Pupkin";
+            //act
+            String result = template.generate(text, data);
+
+            //action
+            Assert.assertThat(result, is(expect));
+           // assertEquals(checked, result);
+        }
+        @Test
+        public void whenTakeTextWithDataShouldReplaceParamsToData1() {
+            //assign
+            Template template = new SimpleGenerator();
+            String text = "Hello, ${name}";
+            Map<String, String> data = new HashMap<>();
+            data.put("${name}", "Dima");
+            String expect = "Hello, Dima";
+            //act
+            String result = template.generate(text, data);
+
+            //action
+            Assert.assertThat(result, is(expect));
+        }
+        @Test
+        public void whenHaveUnnecessaryParamThenException() throws Exception {
+            String text = "${hello}, ${name}!";
+            Map<String, String> params = new HashMap<>();
+            params.put("${name}", "World");
+            params.put("${hello}", "Hello");
+            params.put("${param}", "Param");
+            String expect = "Hello, World!";
+            Template template = new SimpleGenerator();
+            try {
+                String result = template.generate(text, params);
+                Assert.assertThat(result, is(expect));
+            } catch (Exception thrown) {
+                System.out.println(thrown.getMessage());
+                assertNotEquals("", thrown.getMessage());
+            }
+        }
+
+        @Test(expected = Exception.class)
+        public void whenTakeTextWithDataShouldReplaceParamsToData3Error() throws Exception {
+            //assign
+            Template template = new SimpleGenerator();
+            String text = "Hello, ${name} ${surname}";
+            Map<String, String> data = new HashMap<>();
+            data.put("${name}", "Oleg");
+            String expect = "Hello, Oleg";
+            //act
+            String result = template.generate(text, data);
+
+            //action
+            Assert.assertThat(result, is(expect));
+        }
+
+        @Test
+        public void whenTakeTextWithDataShouldReplaceParamsToDataDuplicateStringName() {
+            //assign
+            Template template = new SimpleGenerator();
+            String text = "Hello, my name ${name}";
+            Map<String, String> data = new HashMap<>();
+            data.put("${name}", "Petr");
+            String expect = "Hello, my name Petr";
+            //act
+            String result = template.generate(text, data);
+
+            //action
+            Assert.assertThat(result, is(expect));
+        }
+
+    }

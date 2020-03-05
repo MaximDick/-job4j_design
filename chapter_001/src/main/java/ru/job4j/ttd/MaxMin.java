ae419a99420e726f913603c1ce49a7acd06fde2f
Maxim Dick <maxim1994barca@gmail.com>
2020-03-05 15:47:24 +0300

    Принципы Kiss, Dry и Yagni[#241643]

--- chapter_001/src/main/java/ru/job4j/ttd/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/ttd/MaxMin.java
@@ -3,6 +3,7 @@ package ru.job4j.ttd;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.IntPredicate;
 
 /**
  * @author Maxim Dick (maxim1994barca@gmail.com)
@@ -11,11 +12,33 @@ import java.util.List;
  */
 public class MaxMin {
 
-    public <T> T max(List<T> value, Comparator<T> comparator) {
-        return Collections.max(value, comparator);
+//    public <T> T max(List<T> value, Comparator<T> comparator) {
+//        return Collections.max(value, comparator);
+//    }
+//
+//    public <T> T min(List<T> value, Comparator<T> comparator) {
+//        return Collections.min(value, comparator);
+//    }
+
+    public <T> T max(List<T> values, Comparator<T> comp) {
+        return find(values, comp, x -> x < 0);
     }
 
-    public <T> T min(List<T> value, Comparator<T> comparator) {
-        return Collections.min(value, comparator);
+    public <T> T min(List<T> values, Comparator<T> comp) {
+        return find(values, comp, x -> x > 0);
+    }
+
+    private <T> T find(List<T> values, Comparator<T> comp, IntPredicate select) {
+        if (values == null || values.isEmpty()) {
+            throw new IllegalArgumentException("empty list");
+        }
+        T result = values.get(0);
+        for (int idx = 1; idx < values.size(); idx++) {
+            T next = values.get(idx);
+            if (select.test(comp.compare(result, next))) {
+                result = next;
+            }
+        }
+        return result;
     }
 }

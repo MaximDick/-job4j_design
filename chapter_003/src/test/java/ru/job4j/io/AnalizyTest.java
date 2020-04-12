cde6c5e4e1b0c111145bcc76c30234c719a1646d
Maxim Dick <maxim1994barca@gmail.com>
2020-04-12 20:24:04 +0300

    2. Тестирование IO[#241606]

--- chapter_003/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_003/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,15 +1,25 @@
 package ru.job4j.io;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 
 public class AnalizyTest {
     private Analizy analizy;
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void init() {
@@ -29,5 +39,25 @@ public class AnalizyTest {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void whenServerUnavailableWithTemporaryFolder() throws IOException {
+       File source = folder.newFile("source.txt");
+       File target = folder.newFile("target.txt");
+       try (PrintWriter out = new PrintWriter(source)) {
+           out.println("200 10:56:01");
+           out.println("200 10:57:01");
+           out.println("400 10:58:01");
+           out.println("200 10:59:01");
+           out.println("500 11:01:02");
+           out.println("200 11:02:02");
+       }
+       String sourcePath = source.getAbsolutePath();
+       String targetPath = target.getAbsolutePath();
+       new Analizy().unavailable(sourcePath, targetPath);
+       List<String> actual = Files.readAllLines(Paths.get(targetPath));
+       List<String> expected = List.of("10:58:01-10:59:01;", "11:01:02-11:02:02;");
+       assertThat(actual, is(expected));
     }
 }

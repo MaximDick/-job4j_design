d63125ee35cedd98c42ca4832c722c4a0ba087f2
Maxim Dick <maxim1994barca@gmail.com>
2020-04-21 17:58:07 +0300

    1.Socket.Видео. Доработайте класса ru.job4j.io.EchoServer. Если клиент отправлять запрос http://localhost:9000/?msg=Bye нужно завершить работу сервера.

--- chapter_003/src/main/java/ru/job4j/io/socket/EchoServer.java
+++ chapter_003/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -10,31 +10,33 @@ import java.net.Socket;
 public class EchoServer {
     /** @noinspection checkstyle:InnerAssignment*/
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            boolean flag = false;
-            while (!flag) {
-                System.out.println("Server started");
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        if (!str.equals("GET /?msg=Bye HTTP/1.1")) {
-                            System.out.println(str);
-                        } else {
-                            System.out.println(str);
-                            System.out.println("Server stop");
-                            flag = true;
-                            break;
 
+            try (ServerSocket server = new ServerSocket(9000)) {
+                boolean flag = false;
+                while (!flag) {
+                    System.out.println("Server started");
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        String str = in.readLine();
+                        while (!str.isEmpty()) {
+                            if (!str.equals("GET /?msg=Bye HTTP/1.1")) {
+                                System.out.println(str);
+                                str = in.readLine();
+                            } else {
+                                System.out.println(str);
+                                System.out.println("Server stop");
+                                flag = true;
+                                break;
+
+                            }
                         }
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
 
+                    }
                 }
             }
         }
     }
-}
 

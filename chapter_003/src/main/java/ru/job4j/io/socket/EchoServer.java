e51fd939bdd9409faa582af9cd56f714a07e7e49
Maxim Dick <maxim1994barca@gmail.com>
2020-04-21 21:39:27 +0300

    1. Бот[#241618]

--- chapter_003/src/main/java/ru/job4j/io/socket/EchoServer.java
+++ chapter_003/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -1,6 +1,9 @@
 package ru.job4j.io.socket;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,32 +14,38 @@ public class EchoServer {
     /** @noinspection checkstyle:InnerAssignment*/
     public static void main(String[] args) throws IOException {
 
-            try (ServerSocket server = new ServerSocket(9000)) {
-                boolean flag = false;
-                while (!flag) {
-                    System.out.println("Server started");
-                    Socket socket = server.accept();
-                    try (OutputStream out = socket.getOutputStream();
-                         BufferedReader in = new BufferedReader(
-                                 new InputStreamReader(socket.getInputStream()))) {
-                        String str = in.readLine();
-                        while (!str.isEmpty()) {
-                            if (!str.equals("GET /?msg=Bye HTTP/1.1")) {
-                                System.out.println(str);
-                                str = in.readLine();
-                            } else {
-                                System.out.println(str);
-                                System.out.println("Server stop");
-                                flag = true;
-                                break;
+        try (ServerSocket server = new ServerSocket(9000)) {
+            boolean flag = false;
+            while (!flag) {
+                System.out.println("Server started");
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str = in.readLine();
+                    if (!str.isEmpty()) {
+                        String[] line = str.split(" "); // разбиваем строку на массив строк
+                        int index = line[1].lastIndexOf('='); // позицию  элемента '='
+                        String argument = line[1].substring(index + 1); //извлекаем продстроку из line[1].
+                        String answer;
+                        if ("Hello".equals(argument)) {
+                            answer = "Hello, dear friend.";
 
-                            }
+                        } else if ("Exit".equals(argument)) {
+                            answer = "Exit. Server shut down.";
+
+                            flag = true;
+                        } else {
+                            answer = argument;
+
                         }
-                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write((answer + "\r\n\r\n").getBytes());
+                        System.out.println(str);
                     }
                 }
             }
         }
     }
+}
 

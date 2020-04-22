4e3e3f9816bd9140de8de1ced72da039ef4c0783
Maxim Dick <maxim1994barca@gmail.com>
2020-04-22 13:07:28 +0300

    3. Slf4j - вывод переменных. [#268852]

--- chapter_003/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_003/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,26 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+//        LOG.trace("trace message");
+//        LOG.debug("debug message");
+//        LOG.info("info message");
+//        LOG.warn("warn message");
+//        LOG.error("error message");
+        String name = "Ivan Petrov";
+        int age = 20;
+        double height = 6.0;
+        boolean isMarried = false;
+        char startName = 'I';
+        byte optionA = 0b0100;
+        short shortValue = -32767;
+        long longValue = 100L;
+        float floatValue = 8.5F;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("optionA : {}", optionA);
+        LOG.debug("isMarried : {}", isMarried);
+        LOG.debug("User name begins with : {}", startName);
+        LOG.debug("Short value is : {}", shortValue);
+        LOG.debug("Long value is : {}", longValue);
+        LOG.debug("Float value is : {}", floatValue);
     }
 }
